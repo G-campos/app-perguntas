63d883371018cff910765362fbceb2e6fccb98c6
Gabriel de Campos <gabodecampos@gmail.com>
2023-02-06 22:42:34 -0400

    criação do widget resultado

--- lib/resultado.dart
+++ lib/resultado.dart
@@ -1,17 +1,54 @@
 import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
-  const Resultado({super.key});
+  const Resultado({
+    super.key,
+    required this.pontuacao,
+    required this.quandoReiniciarQuestionario,
+  });
+
+  final int pontuacao;
+  final void Function() quandoReiniciarQuestionario;
+
+  String get fraseResultado {
+    if (pontuacao < 8) {
+      return 'Parabens!';
+    } else if (pontuacao < 12) {
+      return 'Você é bom!';
+    } else if (pontuacao < 16) {
+      return 'Impressionante!';
+    } else {
+      return 'Nível Jedi!';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Parabéns!',
-        style: TextStyle(
-          fontSize: 28,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Center(
+          child: Text(
+            fraseResultado,
+            style: const TextStyle(
+              fontSize: 28,
+            ),
+          ),
         ),
-      ),
+        ElevatedButton(
+          onPressed: quandoReiniciarQuestionario,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+          ),
+          child: const Text(
+            "Reiniciar ?",
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
